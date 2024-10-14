7d1b2aabbab170e4dc649d436b4314cd8e2f9ac0
João Vitor <joaovitorsouza110506@gmail.com>
2024-10-14 20:27:25 -0300

    add class 01 data types

---
+++ 2-JavaScript/class01-data-types/primitive.js
@@ -0,0 +1,10 @@
+let myName = "João Vítor";
+let age = 30;
+const cpf = "123.123.123-12";
+let isAdimin = true;
+
+myName = "João Vítor de Souza Pereira";
+
+console.log(myName);
+console.log(cpf);
+console.log(isAdmin);
---
+++ 2-JavaScript/class01-data-types/structural.js
@@ -0,0 +1,47 @@
+const user = {
+  name: "Goku",
+  age: 51,
+  rule: "Saiyajin",
+  Saiyajin1: true,
+  Saiyajin2: true,
+  Saiyajin3: true,
+};
+const user2 = {
+  name: "Gohan",
+  age: 515,
+  rule: "Saiyajin",
+  Saiyajin1: true,
+  Saiyajin2: true,
+  Saiyajin3: false,
+};
+console.log(typeof user);
+console.log(user);
+console.log(user.name);
+console.log(user.rule);
+
+console.log(user2.name);
+console.log(user2.Saiyajin3);
+
+//array (vector)
+const films = [
+  "Os cavaleiros dos zodíacos",
+  "Dragon Ball",
+  "Naruto",
+  "ThunderCats",
+  "Heeman",
+];
+console.log(films);
+console.log(films[0]);
+console.log(films[1]);
+console.log(films[4]);
+
+const pokemon = [
+  { name: "Charmander", level: 6 },
+  { name: "Bulbasour", level: 5 },
+  { name: "Squirtle", level: 4 },
+];
+
+console.log(pokemon);
+console.log(pokemon[0]);
+console.log(pokemon[1].name);
+console.log(pokemon[2].level);
